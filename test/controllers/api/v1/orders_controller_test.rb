eb3cd66807701753d2f6ee3865bf4fcca7b36db9
ArthurSiqueiraS <asesilva@inf.ufpel.edu.br>
2020-08-02 17:00:19 -0300

    Unit and integration tests for orders

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -1,29 +1,140 @@
 require 'test_helper'
 
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @id = orders.first.id
+  end
+
   test "should get index" do
-    get api_v1_orders_index_url
+    get route
+
     assert_response :success
   end
 
   test "should get show" do
-    get api_v1_orders_show_url
+    get route(@id)
+
     assert_response :success
   end
 
-  test "should get create" do
-    get api_v1_orders_create_url
+  test "should post create" do
+    post route
+
     assert_response :success
   end
 
-  test "should get update" do
-    get api_v1_orders_update_url
+  test "should put update" do
+    put route(@id)
+
     assert_response :success
   end
 
-  test "should get destroy" do
-    get api_v1_orders_destroy_url
+  test "should delete destroy" do
+    delete route(@id)
+
     assert_response :success
   end
 
+  test "index should return an array of orders" do
+    get route
+
+    assert_instance_of Array, response.parsed_body, "Response data is not an array"
+  end
+
+  test "orders should be filterable by number" do
+    get route, params: { number: @id }
+    data = response.parsed_body
+
+    assert data.length == 1
+    assert_equal data.first['id'], @id
+  end
+
+  test "orders should be empty if filtered by non-existing number" do
+    get route, params: { number: 0 }
+    data = response.parsed_body
+
+    assert data.empty?
+  end
+
+  test "orders should be filterable by states" do
+    get route, params: { states: ['pending', 'in_progress'] }
+    data = response.parsed_body
+    filtered_states = data.pluck('state')
+
+    assert filtered_states.include? 'pending'
+    assert filtered_states.include? 'in_progress'
+    assert_not filtered_states.include? 'completed'
+  end
+
+  test "the filters should work together" do
+    get route, params: { number: @id, states: ['pending'] }
+    data = response.parsed_body
+
+    assert data.length == 1 && data.first['id'] == @id && data.first['state'] == 'pending'
+
+    get route, params: { number: @id, states: ['in_progress'] }
+    data = response.parsed_body
+
+    assert data.empty?
+  end
+
+  test "show should return a single order with the required id" do
+    get route(@id)
+
+    assert_equal response.parsed_body['id'], @id
+  end
+
+  test "create should insert a new pending order" do
+    old_count = Order.count
+    post route
+    new_count = Order.count
+
+    assert_equal new_count, old_count + 1
+    assert Order.last.pending?
+  end
+
+  test "updating a pending order should turn it's state to in_progress" do
+    order = orders(:pending)
+    put route(order.id)
+    order = Order.find(order.id)
+
+    assert_not order.pending?
+    assert order.in_progress?
+  end
+
+  test "updating an in_progress order should turn it's state to completed" do
+    order = orders(:in_progress)
+    put route(order.id)
+    order = Order.find(order.id)
+
+    assert_not order.in_progress?
+    assert order.completed?
+  end
+
+  test "updating a completed order should maintain it's state as completed" do
+    order = orders(:completed)
+    put route(order.id)
+    order = Order.find(order.id)
+
+    assert order.completed?
+  end
+
+  test "if deleting an existing id an order should be removed" do
+    old_count = Order.count
+    delete route(@id)
+    new_count = Order.count
+
+    assert_equal new_count, old_count - 1
+    assert_not Order.pluck(:id).include? @id
+  end
+
+  private
+
+  def route(id=nil)
+    route = '/api/v1/orders'
+
+    route += "/#{id}" if id
+
+    route
+  end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be created with pending state" do
+    order = Order.create
+
+    assert order.pending?, "Initial state is not pending"
+  end
+
+  test "order id should be an integer" do
+    order = Order.create
+
+    assert_instance_of Integer, order.id, "Order id is not an integer"
+  end
+
+  test "order can't be directly updated" do
+    order = Order.create
+
+    assert_raises AASM::NoDirectAssignmentError do
+      order.update(state: 'in_progress')
+    end
+  end
+
+  test "if order is pending, it can be started" do
+    order = orders(:pending)
+
+    assert order.pending? && order.may_start?, "Pending order can't be started"
+  end
+
+  test "if order is pending, it can't be completed" do
+    order = orders(:pending)
+
+    assert order.pending? && !order.may_complete?, "Pending order can be completed"
+  end
+
+  test "if order is in progress, it can be completed" do
+    order = orders(:in_progress)
+
+    assert order.in_progress? && order.may_complete?, "Order in progress can't be completed"
+  end
+
+  test "if order is in progress, it can't be started" do
+    order = orders(:in_progress)
+
+    assert order.in_progress? && !order.may_start?, "Order in progress can be started"
+  end
+
+  test "if order is completed, it can't be started" do
+    order = orders(:completed)
+
+    assert order.completed? && !order.may_start?, "Completed order can be started"
+  end
+
+  test "if order is completed, it can't be completed" do
+    order = orders(:completed)
+
+    assert order.completed? && !order.may_complete?, "Completed order can be completed"
+  end
 end
